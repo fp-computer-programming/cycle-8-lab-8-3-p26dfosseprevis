a5527a4d123f0f3731987ef6dcdfcfde5f77c0d8
Dylan Fosse Previs <p26dfosseprevis@fairfieldprep.org>
2023-12-01 11:13:31 -0500

    Finished assinment

--- Lab_8-3.py
+++ Lab_8-3.py
@@ -13,3 +13,14 @@ def sum_to(n):
 
 The function should be able to have an integer passed to it and return the sum of all the values from 1 to that integer
 """
+def sum_to(n):
+	total = 0
+	i = 0
+	while i <= n:
+		total += i
+		i += 1
+	return total
+
+print(sum_to(3))
+print(sum_to(7))
+print(sum_to(4))
